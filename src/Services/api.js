2bc9d94c94ae06d72ff90ab3f8e8cceb9e9b5236
ABHINENDRA PRATAP SINGH <abhinendrapratap2016@gmail.com>
2023-07-10 20:00:04 +0530

    api fixed

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_KEY = '67b85dfd1b4e6cfc165362f8cf4bce6d';
-const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+const API_KEY = process.env.API_KEY;
+const API_URL = process.env.API_URL;
 
 export const getWeather = async (city, country) => {
     try {
